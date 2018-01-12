f2c6b3283cb0a242809ee16d3493cd01cb4595b2
TheBaileyBrew <matthew.bailey@hthackney.com>
2018-01-12 12:04:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/helloandroid/AboutUdacity.java
@@ -0,0 +1,36 @@
+package com.example.android.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class AboutUdacity extends AppCompatActivity {
+
+    ImageButton androidButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_udacity);
+
+    }
+    public void buttonFB (View view) {
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/udacity"));
+        startActivity(browserIntent);
+    }
+    public void buttonTube (View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/udacity"));
+        startActivity(browserIntent);
+    }
+    public void buttonTweet (View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.twitter.com/udacity"));
+        startActivity(browserIntent);
+    }
+    public void buttonWWW (View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.udacity.com"));
+        startActivity(browserIntent);
+    }
+}
