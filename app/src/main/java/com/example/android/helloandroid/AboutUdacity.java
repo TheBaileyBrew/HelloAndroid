b5cd329842ce19cfb6ce5b3a5462b44f4e0d04e3
Matthew B <matthew.bailey@hthackney.com>
2018-01-13 14:50:11 -0500

    New Updates

--- app/src/main/java/com/example/android/helloandroid/AboutUdacity.java
+++ app/src/main/java/com/example/android/helloandroid/AboutUdacity.java
@@ -1,22 +1,27 @@
 package com.example.android.helloandroid;
 
+import android.app.MediaRouteButton;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
 
 public class AboutUdacity extends AppCompatActivity {
 
-    ImageButton androidButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_udacity);
 
-    }
+        }
+
+
     public void buttonFB (View view) {
         Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/udacity"));
         startActivity(browserIntent);
@@ -33,6 +38,9 @@ public class AboutUdacity extends AppCompatActivity {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.udacity.com"));
         startActivity(browserIntent);
     }
+
+
+
 
 
 }
