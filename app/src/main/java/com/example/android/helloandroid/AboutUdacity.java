a15f91d70d93a860f270bd02a0f09864fdbdba78
Matthew B <matthew.bailey@hthackney.com>
2018-01-13 17:24:42 -0500

    New Updates

--- app/src/main/java/com/example/android/helloandroid/AboutUdacity.java
+++ app/src/main/java/com/example/android/helloandroid/AboutUdacity.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -13,34 +15,82 @@ import android.widget.LinearLayout;
 
 public class AboutUdacity extends AppCompatActivity {
 
+    Button myButton;
+    View myView;
+    boolean isUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_udacity);
 
-        }
+        myView = findViewById(R.id.my_view);
+        myButton = findViewById(R.id.my_button);
+
+        //set as invisible
+        myView.setVisibility(View.INVISIBLE);
+        isUp = false;
+    }
 
 
-    public void buttonFB (View view) {
-        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/udacity"));
+    public void buttonFB(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/udacity"));
         startActivity(browserIntent);
     }
-    public void buttonTube (View view) {
+
+    public void buttonTube(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/udacity"));
         startActivity(browserIntent);
     }
-    public void buttonTweet (View view) {
+
+    public void buttonTweet(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.twitter.com/udacity"));
         startActivity(browserIntent);
     }
-    public void buttonWWW (View view) {
+
+    public void buttonWWW(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.udacity.com"));
         startActivity(browserIntent);
     }
 
+    // slide the view from below itself to the current position
+    public void slideUp(View view) {
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 100,                 // fromXDelta
+                Animation.RELATIVE_TO_PARENT, 100,                 // toXDelta
+                Animation.ABSOLUTE, 100,  // fromYDelta
+                Animation.ABSOLUTE, 100);                // toYDelta
+        animate.setDuration(50);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
 
+    // slide the view from its current position to below itself
+    public void slideDown(View view) {
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight()); // toYDelta
+        animate.setDuration(50);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
 
-
-
+    public void onSlideViewButtonClick(View view) {
+        if (isUp) {
+            slideDown(myView);
+            myButton.setText("SHOW LINKS");
+        } else {
+            slideUp(myView);
+            myButton.setText("HIDE LINKS");
+        }
+        isUp = !isUp;
+    }
 }
+
+
+
+
+
